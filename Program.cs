e8e7332a2c5549dddf28b18cf641d874b38fbbc5
Amanda Morgan <69015861+thepandaknows@users.noreply.github.com>
2024-01-28 14:51:50 -0800

    Update Program.cs
    Added program features menu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
 
-// testing
+// menu of features for steps 2-5
+Console.WriteLine("Menu:");
+Console.WriteLine("1) Output a random integer between 1-10");
+Console.WriteLine("2) Show today's date");
+Console.WriteLine("3) Output a random dinosaur name");
+Console.WriteLine("4) Input a string, then perform a random action on that string");
+Console.WriteLine("Enter your choice (1-4): ");
