1f0d02cb3619953eb6c6fa20946320b3fd03a792
Amanda Morgan <by.amanda.morgan@gmail.com>
2024-03-03 16:23:15 -0800

    Co-authored-by: GamesBrewing <GamesBrewing@users.noreply.github.com>

--- Assignment1.Tests/UnitTest1.cs
+++ Assignment1.Tests/UnitTest1.cs
@@ -11,25 +11,39 @@ namespace Assignment1.Tests
         {
             var test = new Program();
             int x = 0;
-           x = test.Option1();
+            x = test.Option1();
 
             Assert.IsTrue (x<=10 & x>=1); // Test if value is in range
-           Assert.IsNotNull (x);  //Make sure RNG isn't returning null
+            Assert.IsNotNull (x);  //Make sure RNG isn't returning null
 
         }
 
+        /* [TestMethod]
+         public void TestOption2()
+         {
+             var test = new Program();
+            string x = "";
+             x = test.Option2();
+
+             Assert.AreEqual("2/18/2024", x); // Have to change string to the today's date to test
+             Assert.AreNotEqual("00/00/0000", x);
+                    
+
+         }*/
+
+        // Option to dynamically generate "today's" date within the unit test, then compare to date in Program.cs
         [TestMethod]
         public void TestOption2()
         {
             var test = new Program();
-            string x = "";
-            x = test.Option2();
+            string expectedDate = DateTime.Today.ToShortDateString();
+            string actualDate = test.Option2();
 
-            Assert.AreEqual("2/18/2024", x); // Have to change string to the today's date to test
-            Assert.AreNotEqual("00/00/0000", x);
-                    
-
+            // Assert
+            Assert.AreEqual(expectedDate, actualDate, $"Expected date: {expectedDate}, Actual date: {actualDate}");
+            Assert.AreNotEqual("00/00/0000", actualDate, "Actual date should not be a placeholder for an invalid date.");
         }
+
 
         [TestMethod]
         public void TestOption3()
